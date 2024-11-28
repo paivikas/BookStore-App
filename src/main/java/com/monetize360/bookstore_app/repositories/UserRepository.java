16d06b833611e78fc2873b23138b5baddf29b9f6
Swasthik-Shetty <Swasthik.Shetty@monetize360.com>
2024-11-28 12:50:21 +0530

    added jpa structure

--- src/main/java/com/monetize360/bookstore_app/repositories/UserRepository.java
+++ src/main/java/com/monetize360/bookstore_app/repositories/UserRepository.java
@@ -1,5 +1,5 @@
 package com.monetize360.bookstore_app.repositories;
-import com.monetize360.bookstore_app.domain.User;
+import com.monetize360.bookstore_app.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<Users, UUID> {}
--- src/main/java/com/monetize360/bookstore_app/service/UserServiceImpl.java
+++ src/main/java/com/monetize360/bookstore_app/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package com.monetize360.bookstore_app.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.monetize360.bookstore_app.domain.User;
+import com.monetize360.bookstore_app.domain.Users;
 import com.monetize360.bookstore_app.dto.UserDto;
 import com.monetize360.bookstore_app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +18,22 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto insertUser(UserDto userDTO) {
-        User user = objectMapper.convertValue(userDTO, User.class);
-        user.setUserID(UUID.randomUUID());
-        user = userRepository.save(user);
-        return objectMapper.convertValue(user, UserDto.class);
+        Users users = objectMapper.convertValue(userDTO, Users.class);
+        users.setUserId(UUID.randomUUID());
+        users = userRepository.save(users);
+        return objectMapper.convertValue(users, UserDto.class);
 
     }
 
     @Override
     public UserDto updateUser(UserDto userDTO) {
-        User user = userRepository.findById(userDTO.getUserID()).orElseThrow(() -> new RuntimeException("User not found"));
-        user.setName(userDTO.getName());
-        user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
-        user.setDeleted(userDTO.isDeleted());
-        User updatedUser = userRepository.save(user);
-        return objectMapper.convertValue(updatedUser, UserDto.class);
+        Users users = userRepository.findById(userDTO.getUserID()).orElseThrow(() -> new RuntimeException("Users not found"));
+        users.setName(userDTO.getName());
+        users.setEmail(userDTO.getEmail());
+        users.setPassword(userDTO.getPassword());
+        users.setDeleted(userDTO.isDeleted());
+        Users updatedUsers = userRepository.save(users);
+        return objectMapper.convertValue(updatedUsers, UserDto.class);
 
     }
 
@@ -44,11 +44,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteUser(UUID id) {
-        Optional<User> optionalUser = userRepository.findById(id);
+        Optional<Users> optionalUser = userRepository.findById(id);
         if (optionalUser.isPresent()) {
-            User user = optionalUser.get();
-            user.setDeleted(true);
-            userRepository.save(user);
+            Users users = optionalUser.get();
+            users.setDeleted(true);
+            userRepository.save(users);
         } else {
             throw new RuntimeException("Contact not found");
         }
